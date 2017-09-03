abdfcd2490b8f9ef26575a1e913973b3decdc9b1
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2017-09-03 22:10:02 +0530

    Create Boundary Trees.cpp

---
+++ Boundary Trees.cpp
@@ -0,0 +1,57 @@
+/*Boundary Trees (Id-2978)
+Certain trees are planted in R rows and C columns in a rectangular field. The current height of the trees are passed as the input. In a month, each tree normally grows by N inches. But the trees on the edge of the rectangular field can grow double the normal rate as they get more sunlight when compared to other trees.
+
+The program must print the height of the trees after M months.
+
+Input Format:
+The first line contains the values of R and C separated by a space.
+Next R lines will contain C values each which represents the current height of the trees, with the values separated by a space.
+The next line will contain the value of N (the normal growth rate) and M (the number of months) separated by a space.
+
+Output Format:
+R lines will contain C values each representing the height of the trees after M months.
+
+Boundary Conditions:
+1 <= R, C <= 20
+1 <= N <= 100
+1 <= M <= 12
+
+Example Input/Output 1:
+Input:
+4 3
+5 3 6
+9 4 10
+7 8 7
+8 9 9
+1 3
+
+Output:
+11 9 12
+15 7 16
+13 11 13
+14 15 15
+*/
+
+
+#include <iostream>
+ 
+using namespace std;
+int main(int argc, char** argv){
+      int m,n,k,o,a[100][100];
+      cin>>m>>n;
+      for(int i=0;i<m;i++)
+          for(int j=0;j<n;j++)
+              cin>>a[i][j];
+      cin>>k>>o;
+      for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+                  if(i==0||j==0||i==m-1||j==n-1)
+                      cout<<a[i][j]+(o*2)*k<<" ";
+                  else
+                      cout<<a[i][j]+(o*k)<<" ";
+              }    
+
+          cout<<endl;
+          }
+}
+
