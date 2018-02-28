97328cc92c003b11fa6da5e4858e5ac773b4d765
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-02-28 08:01:04 +0530

    Create Remove First And Last Till End.cpp

---
+++ Remove First And Last Till End.cpp
@@ -0,0 +1,31 @@
+/*
+Remove First And Last Till End
+
+Given a string S, print the string S removing first and last character till no characters are left.
+ 
+Boundary Condition :
+1 <= Length of string S <= 1000
+
+Input Format:
+The first line contains S
+
+Example Input/Output 1:
+Input:
+water
+
+Output :
+water
+ate
+t
+
+
+Example Input/Output 2:
+Input:
+keyboard
+
+Output :
+keyboard
+eyboar
+yboa
+bo
+*/
