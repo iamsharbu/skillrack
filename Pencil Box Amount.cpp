e8454eb5b79ba30316c8d0149e59d651893bd7f8
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-03-10 19:47:43 +0530

    Create Pencil Box Amount.cpp

---
+++ Pencil Box Amount.cpp
@@ -0,0 +1,40 @@
+/*
+Pencil Box Amount
+
+Amit is working as a teacher in a school. He wants to buy N pencil boxes which cost Rs.P each. Given N and P, find the total amount A which Amit has to pay.
+
+Input Format:
+The first line contains N.
+The second line contains P.
+
+Output Format:
+The first line contains A (the total amount to pay).
+
+Boundary Conditions:
+1 <= N <= 99999
+1 <= P <= 1000
+
+Example Input/Output 1:
+Input:
+5
+10
+
+Output:
+50
+
+Example Input/Output 2:
+Input:
+11
+20
+
+Output:
+220
+*/
+
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv){
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b;
+}
