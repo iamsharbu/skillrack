970f5a8e4e2ea3e5b73b0e95ca1b5c2bd9a9eeb7
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-05-18 08:48:30 +0530

    Create Distance From Origin.cpp

---
+++ Distance From Origin.cpp
@@ -0,0 +1,40 @@
+/*
+Distance From Origin
+
+A point in a graph (x,y) is given as input. The program must print the distance between the given point and the origin with precision up to two decimal places.
+
+Boundary Condition(s):
+1 <= x, y <= 100
+
+Input Format:
+The first line contains the value of x and y separated by space(s).
+
+Output Format:
+The first line contains the distance with precision up to two decimal places.
+
+Example Input/Output 1:
+Input:
+3 3
+
+Output:
+4.24
+
+Example Input/Output 2:
+Input:
+4 0
+
+Output:
+4.00
+*/
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    float a,b,c;
+    cin>>a>>b;
+    printf("%0.2f"sqrt((a*a)-0+(b*b)-0));
+
+
+}
