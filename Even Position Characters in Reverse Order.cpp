1966084ba45087d7759706e69af5d297620e0d3a
Muhamed Salih Seyed Ibrahim <30931525+MuhamedSalihSeyedIbrahim@users.noreply.github.com>
2018-06-03 05:20:15 +0530

    Create Even Position Characters in Reverse Order.cpp

---
+++ Even Position Characters in Reverse Order.cpp
@@ -0,0 +1,39 @@
+/*
+
+Even Position Characters in Reverse Order
+
+A string S is passed as input. The program must print the characters present at the even positions of the string in reverse order.
+
+Boundary Condition(s):
+2 <= Length of String <= 1000
+
+Input Format:
+The first line contains the string S.
+
+Output Format:
+The first line contains the characters at even positions in reverse order.
+
+Example Input/Output 1:
+Input:
+independent
+
+Output:
+ndeen
+
+Example Input/Output 2:
+Input:
+computer
+
+Output:
+rtpo
+
+*/
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv)
+    string a;
+    cin>>a;
+    for(int i=a.length()-1;i>=0;i--){
+        ((i+1)%2==0)?cout<<a[i]:cout<<"";
+    }
+}
